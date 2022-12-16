4e0961c6bda9496476bf1b1770edaf46c4a48373
pawans <pawans@ICS-LAP-3101>
2022-12-16 16:12:46 +0530

    New test Class

---
+++ Test1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace infiniteDesignPatternFactory
+{
+    public class Test
+    {
+        public Test()
+        {
+            Console.WriteLine("hello");
+        }
+
+        static void Main(string[] args)
+        {
+            Test test = new Test();
+
+        }
+    }
+}
